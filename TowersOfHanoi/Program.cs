4c2f32fc8b89bc393bda65a3513d3f22a950cfe9
Curtis Sapp <curtsapp@hotmail.com>
2016-07-04 13:32:28 -0700

    Clean up time
    Added comments
    Removed unnecessary spaces

--- TowersOfHanoi/Program.cs
+++ TowersOfHanoi/Program.cs
@@ -15,29 +15,25 @@ namespace TowersOfHanoi
         {
 
             Nonstatic Helper = new Nonstatic();
+            //Promp the user for input
             Console.Write("How many disks would you like to calculate?");
             String numberOfDisksToGenerateString = Console.ReadLine();
             ulong numberOfDisksToGenerateNum = UInt64.Parse(numberOfDisksToGenerateString);
-
+            //Number of moves is calculted with 2^n - 1.
             double numberOfMoves = Math.Pow(2,numberOfDisksToGenerateNum) - 1;
             Console.WriteLine("It will take {0} moves to finish the puzzle.", numberOfMoves);
             Console.WriteLine("-------------------MOVES----------------------");
             List<Disk> StartStack = new List<Disk>();
+            //Build the original stack of disks, incrementing size each time.
             for (ulong i = 0; i < numberOfDisksToGenerateNum; i++)
             {
                 StartStack.Add(new Disk(i));
             }
+            //Begins our recursive loop
             Helper.MoveStack(StartStack, Post.Start, Post.Mid, Post.End);
 
             String temp = Console.ReadLine();
         }
-
-
-        
-
-
-
-    
     }
 
     public class Nonstatic
@@ -46,6 +42,7 @@ namespace TowersOfHanoi
 
         public void MoveStack(List<Disk> stack, Post start, Post aux, Post end)
         {
+            //Recrusive loop exit
             if (stack.Count == 1)
             {
                 Console.WriteLine("Move {0}, to, {1} post. Step {2}", stack[0].Size, end, StepCount);
@@ -54,6 +51,7 @@ namespace TowersOfHanoi
             }
             else
             {
+
                 List<Disk> shortenedStack = new List<Disk>(stack);
                 List<Disk> lastDiskStack = new List<Disk>();
                 lastDiskStack.Add(stack[stack.Count() - 1]);
@@ -80,11 +78,5 @@ namespace TowersOfHanoi
     {
         Start, Mid, End,
     
-
     }
-    
-
-
-
-
 }
